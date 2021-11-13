86cfe92d80e08aad695643ec195fb76710567f1e
renezer <tankelevicius@gmail.com>
2021-11-13 15:10:13 +0200

    Task2 lame solution 520points

--- Task2/spaceBattle.js
+++ Task2/spaceBattle.js
@@ -14,17 +14,8 @@ const browser = new Builder()
     function play() {
         browser.findElement(By.css("div#splashScreen  a")).click();
     }
-/*
-    async function shoot() {
-        for(i=0;i<2;i++){
-            browser.findElement(By.css("body")).sendKeys(Key.SPACE);
-            i--;
-        }
-    }
-*/
     open();
     play();
-    //shoot();
 
     function shoot(i) {
         setTimeout(() => {
